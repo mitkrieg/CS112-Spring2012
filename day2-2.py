d65fd910db40389aa994be57572fabfba4041bcc
Mitchell Krieger <miak11@hampshire.edu>
2012-01-30 11:26:20 -0500

    I updated readme

---
+++ day2-2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+day2-2.py
+
+A first program
+"""
+
+print "hello nurse!"
+print "Isn't this a wonderful day?!"
+print "lots of math to come"
+print "1+3=",1+3 #allows you to join mutiple things
+print "2*8=",2*8
+print "that was uber exciting"
