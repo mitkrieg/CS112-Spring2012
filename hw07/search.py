b4e52dc309bf40caeac7b11227cd8c54f75e53fb
Mitchell Krieger <miak11@hampshire.edu>
2012-02-20 14:55:25 -0500

    hw07 done

--- hw07/search.py
+++ hw07/search.py
@@ -6,23 +6,26 @@ This was supposed to be a binary search algorithm but it isn't working...
 I used the Iterative implementation from here:
     http://en.wikipedia.org/wiki/Binary_search_algorithm
 """
+import hwtools
 from hwtools import input_nums
 
-nums=inputNums()
-sorted(nums)
+nums=input_nums()
+sortnums=sorted(nums)
 print "I have sorted your numbers"
-x=raw_input("Which number should I find: ")
-m=1
-M=len(nums)-1
+print sortnums
+x=int(raw_input("Which number should I find: "))
+m=0
+M=len(sortnums)-1
 while M>=m:
-    md=M+m/2
-    if nums[md]==x:
+    md=int((M+m)/2)
+    if sortnums[md]==x:
         break
-    elif x>nums[md]:
+    elif sortnums[md]<=x:
        m=md+1
-     else:
-        M-=1
-if nums[md]=x:
-    print "Found", x "at", md
-else
+    elif sortnums[md]>x:
+        M=md-1
+if sortnums[md]==x:
+    print "Found", x, "at", md
+else:
     print "Could not find", x
+print sortnums
